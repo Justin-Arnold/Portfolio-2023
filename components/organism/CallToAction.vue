f6e155dce34fc369a931dbc0ba7a0cb9f7b487e4
Justin Arnold <justin.arnold@programmer.net>
2022-10-24 23:08:23 -0400

    Few style changes to the CTA

--- components/organism/CallToAction.vue
+++ components/organism/CallToAction.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="h-full grid place-items-center">
-        <div class="flex flex-col gap-4 text-white">
-            <h2 class="text-5xl font-bold">I am Attention Grabbing</h2>
-            <h4 class="font-thin font-serif text-md text-gray-300">And here in the dark is where I writhe in agony as I create mystery and intrigue for all those who seek it.</h4>
-            <span class="px-3 py-1 border-2 border-white rounded w-[fit-content]">Don't Click</span>
+        <div class="flex flex-col gap-5 text-white">
+            <h2 class="text-6xl font-bold">I am Attention Grabbing</h2>
+            <h4 class="font-thin font-serif text-sm text-gray-300 max-w-[70ch]">And here in the dark is where I writhe in agony as I create mystery and intrigue for all those who seek it.</h4>
+            <span class="px-3 py-1 border-2 border-white rounded w-[fit-content] text-sm">Don't Click</span>
         </div>
     </div>
 </template>
