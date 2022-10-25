24b3489efae12e96fdb2b4767465cf3734cf7a28
Justin Arnold <justin.arnold@programmer.net>
2022-10-24 22:55:37 -0400

    fixed the header balance
    the middle nav wasn't centered on the viewport and now it is

--- components/organism/SiteHeader.vue
+++ components/organism/SiteHeader.vue
@@ -1,14 +1,16 @@
 <template>
     <div class="flex justify-between items-center h-10 sticky top-0 text-white">
-        <div>
-            <h2 class="font-bold text-lg">Justin Arnold</h2>
+        <div class="grow basis-0">
+            <h2 class="font-bold text-lg" >Justin Arnold</h2>
         </div>
         <div class="flex justify-center items-center gap-4">
             <h2 class=" text-lg">Blog</h2>
             <h2 class=" text-lg">Projects</h2>
             <h2 class=" text-lg">Other</h2>
         </div>
-        <div>☠️</div>
+        <div class="grow basis-0">
+            <div class="float-right">☠️</div>
+        </div>
     </div>
 </template>
 
