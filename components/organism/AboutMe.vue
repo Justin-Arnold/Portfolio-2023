654622d0c5b5cb09d6d873652b16ad11cea5934f
Justin Arnold <justin.arnold@programmer.net>
2022-11-08 18:44:01 -0500

    Basic skeleton about section

--- components/organism/AboutMe.vue
+++ components/organism/AboutMe.vue
@@ -1,6 +1,18 @@
 <template>
-    Hello World
-    <img class="h-40 w-40" src="/img/headshot.png"/>
+    <div class="grid place-items-center h-full text-white">
+        <div class="flex">
+            <div class="max-w-[45ch]">
+                <h2 class="text-lg font-bold">Hi, I'm Justin</h2>
+                <p class="text-neutral-400">Software Engineer at <span class="text-green-500">Gradecam</span></p>
+                <p class="py-4">
+                    I'm baby do kickstarter knausgaard meditation ullamco drinking vinegar. Selvage yr pabst kickstarter hexagon. Banh mi artisan microdosing poutine adipisicing cray voluptate etsy yuccie tofu vegan pok pok master cleanse nostrud laboris. Biodiesel celiac VHS Brooklyn messenger bag green juice fit street art in tacos succulents bodega boys ut. Kogi irony kombucha eu celiac, craft beer bodega boys gluten-free taxidermy viral tilde proident raw denim. Readymade cloud bread ut marfa praxis mixtape affogato commodo schlitz nulla before they sold out bicycle rights sed.
+                </p>
+            </div>
+            <div class="grid place-items-center px-4">
+                <img class="h-80 w-80 rounded border-2 border-neutral-light" src="/img/headshot.png"/>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script setup lang="ts">
