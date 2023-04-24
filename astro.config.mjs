49ecdb76c00a0d60761a3d6aaa237a7d5378391b
Justin Arnold <justin.arnold@programmer.net>
2023-04-23 22:39:38 -0400

    updated file structure so images work

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,11 +8,15 @@ import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: {
-      applyBaseStyles: false
-    }
-  }), vue()]
+    experimental: {
+        assets: true
+    },
+    integrations: [
+        tailwind({
+            config: {
+                applyBaseStyles: false
+            }
+        }),
+        vue()
+    ]
 });
