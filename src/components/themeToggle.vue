f6e6060d5cfb513d5b5790f3c7b95304cf88eb74
Justin Arnold <justin.arnold@programmer.net>
2023-05-03 16:17:44 -0400

    added functionality to theme toggle

---
+++ src/components/themeToggle.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import { onBeforeMount, ref, watchEffect } from 'vue';
+
+const isDark = ref(false);
+
+watchEffect(() => {
+    if (!isDark.value) {
+        console.log('dark');
+        document.documentElement.classList.add('dark');
+    } else {
+        console.log('light');
+        document.documentElement.classList.remove('dark');
+    }
+});
+</script>
+
+<template>
+    <input v-model="isDark" class="appearance-none outline-none cursor-pointer w-[var(--size)] h-[var(--size)] rounded-full text-slate-50 transition-all duration-500 checked:scale-75" type="checkbox">
+</template>
+
+
+<style scoped>
+input {
+	--size: 2rem;
+	box-shadow: inset calc(var(--size) * 0.33) calc(var(--size) * -0.25) 0;
+}
+input:checked {
+	--ray-size: calc(var(--size) * -0.4);
+	--offset-orthogonal: calc(var(--size) * 0.65);
+	--offset-diagonal: calc(var(--size) * 0.45);
+	box-shadow: inset 0 0 0 var(--size),
+                calc(var(--offset-orthogonal) * -1) 0 0 var(--ray-size),
+                var(--offset-orthogonal) 0 0 var(--ray-size),
+                0 calc(var(--offset-orthogonal) * -1) 0 var(--ray-size),
+                0 var(--offset-orthogonal) 0 var(--ray-size),
+                calc(var(--offset-diagonal) * -1) calc(var(--offset-diagonal) * -1) 0 var(--ray-size),
+                var(--offset-diagonal) var(--offset-diagonal) 0 var(--ray-size),
+                calc(var(--offset-diagonal) * -1) var(--offset-diagonal) 0 var(--ray-size),
+                var(--offset-diagonal) calc(var(--offset-diagonal) * -1) 0 var(--ray-size);
+}
+
+</style>
