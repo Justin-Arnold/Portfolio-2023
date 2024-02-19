5eb6a7bbeff1eef0e677eef93e57961646bfbf10
Justin Arnold <justin.arnold@programmer.net>
2024-02-19 16:51:48 -0500

    feat: Use storyblok for the projects section

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,38 @@ import sanity from "@sanity/astro";
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vue from "@astrojs/vue";
+import basicSsl from '@vitejs/plugin-basic-ssl';
+import tailwindcssNesting from 'tailwindcss/nesting';
+import storyblok from '@storyblok/astro';
+import { loadEnv } from 'vite';
 
 export default defineConfig({
-    // experimental: {
-    //     assets: true
-    // },
+    vite: {
+        plugins: [basicSsl()],
+        server: {
+            https: true
+        },
+        css: {
+            postcss: {
+                plugins: [tailwindcssNesting()]
+            }
+        }
+    },
     integrations: [
         tailwind({
             config: {
                 applyBaseStyles: false
+            }
+        }),
+        storyblok({
+            accessToken: process.env.STORYBLOK_TOKEN,
+            bridge: import.meta.env.VITE_ENVIRONMENT === "preview" ? true : false,
+            apiOptions: {
+                region: "us"
+            },
+            components: {
+                project_list: "components/storyblok/ProjectList",
+                project: "components/storyblok/ProjectListCard"
             }
         }),
         vue(),
